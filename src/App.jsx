56653a83bd5d42a4d6e8d7513b14f83b338a9120
Yared-Betemariam <149861225+Yared-Betemariam@users.noreply.github.com>
2023-12-01 17:46:28 +0300

    creating the necesarrry files for the starter code
    i just made the about and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div className="text-white bg-cyan-900">TEmplate</div>
+    <BrowserRouter>
+      <header>
+        <Link to='/' >{"(MOONLIGE)"}</Link>
+        <nav>
+          <Link to='/about'>About</Link>
+          <Link>Our Rockets</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
