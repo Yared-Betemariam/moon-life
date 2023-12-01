35366cf2a31773022318e1b98bedd83e94b0726d
Yared-Betemariam <149861225+Yared-Betemariam@users.noreply.github.com>
2023-12-01 22:12:24 +0300

    setting up the paths for the router

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,16 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout /> }>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/rockets' element={<Rockets />}/>
-          <Route path='/rockets/:id' element={<RocketDetail />}/>
-          <Route path='/host' element={<HostLayout />}>
-            <Route path='/host' element={<Dashboard />}/> 
-            <Route path='/host/income' element={<Income />}/>
-            <Route path='/host/reviews' element={<Reviews />}/>
+        <Route path='/' element={<Layout /> }>
+          <Route index element={<Home />}/>
+          <Route path='about' element={<About />}/>
+          <Route path='rockets' element={<Rockets />}/>
+          <Route path='rockets/:id' element={<RocketDetail />}/>
+
+          <Route path='host' element={<HostLayout />}>
+            <Route index element={<Dashboard />}/> 
+            <Route path='income' element={<Income />}/>
+            <Route path='reviews' element={<Reviews />}/>
           </Route>
           
         </Route>
